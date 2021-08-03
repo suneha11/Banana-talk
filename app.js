67b15c7e8840cbc0f13b242c97a6f3070c10d895
suneha11 <suneha.ss1502@gmail.com>
2021-08-04 04:11:16 +0530

    done

--- app.js
+++ app.js
@@ -2,9 +2,34 @@ var buttonTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL ="https://api.funtranslations.com/translate/shakespeare.json"
+
+function getTranslationURL(text) {
+    
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured",error);
+    alert("Something wrong in server : Try again after sometime.")
+}
+
 function clickHandler(){
-    outputDiv.innerText="ajjaaajxb "+textInput.value;
-};
+    
+    var inputText = textInput.value; //taking input
+
+   //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; //output
+            })
+        .catch(errorHandler)
+
+}; 
 
 buttonTranslate.addEventListener("click",clickHandler )
 
